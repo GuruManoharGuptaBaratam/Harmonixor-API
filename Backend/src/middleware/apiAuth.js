0b27812218fbf8af4b7ef04fb12f39e54690dc71
GuruManoharGuptaBaratam <manoharguptabaratam@gmail.com>
2025-09-15 22:01:27 +0530

    added user.js import in auth.js

--- Backend/src/middleware/apiAuth.js
+++ Backend/src/middleware/apiAuth.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 async function apiKeyAuth(req, res, next) {
   const apiKey = req.query.KEY; // API key from URL
   if (!apiKey) return res.status(401).json({ error: "API key missing" });
