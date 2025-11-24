7e709536abfec8a5e7e613ee35fc75781957746e
GuruManoharGuptaBaratam <manoharguptabaratam@gmail.com>
2025-11-24 13:24:22 +0530

    Added a test docker file

--- Backend/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit immediately if a command fails
-set -o errexit
-
-# Update system packages and install ffmpeg
-apt-get update && apt-get install -y ffmpeg python3 python3-pip
-
-# Install yt-dlp globally
-pip3 install yt-dlp
