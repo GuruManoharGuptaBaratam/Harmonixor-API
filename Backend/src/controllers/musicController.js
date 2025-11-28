924c1f0b92fd14cdae196fac1a5a07c8954f0ccf
GuruManoharGuptaBaratam <manoharguptabaratam@gmail.com>
2025-11-28 15:22:20 +0530

    testing working local logic-03

--- Backend/src/controllers/musicController.js
+++ Backend/src/controllers/musicController.js
@@ -51,9 +51,27 @@ try {
 
 async function handleSongStream(req, res, songUrlParam) {
   const id = songUrlParam || req.query.songUrl;
-  const url = `https://www.youtube.com/watch?v=${id}`;
+  const APIKEY = req.apiKey;
+    if (!APIKEY) return res.status(401).json({ error: "API key missing" });
 
-  exec(`yt-dlp --extractor-args "youtube:player_client=tv" -g "https://www.youtube.com/watch?v=${id}"`, (err, stdout) => {
+    const user = await User.findOne({ where: { apiKey: APIKEY } });
+    if (!user) return res.status(403).json({ error: "Invalid API key" });
+
+    const cookieBase64 = user.cookieFile;
+    if (!cookieBase64) return res.status(400).json({ error: "No cookie found for this user" });
+
+    const buffer = Buffer.from(cookieBase64, "base64");
+
+    const cookiesDir = path.join(__dirname, "../../UserCookies");
+    if (!fs.existsSync(cookiesDir)) {
+      fs.mkdirSync(cookiesDir, { recursive: true });
+    }
+
+  const tempCookiePath = path.join(cookiesDir, `temp_cookie_${Date.now()}.txt`);
+  await fs.promises.writeFile(tempCookiePath, buffer);
+  // const url = `https://www.youtube.com/watch?v=${id}`;
+
+  exec(`yt-dlp --cookies "${tempCookiePath}" -g "https://www.youtube.com/watch?v=${id}"`, (err, stdout) => {
     if (err) return res.status(500).json({ error: err.message });
 
     const [videoUrl, audioUrl] = stdout.trim().split("\n");
