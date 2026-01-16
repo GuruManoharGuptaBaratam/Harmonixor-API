c5088e2526c6b4bc726a755addd95f98404a1257
GuruManoharGuptaBaratam <manoharguptabaratam@gmail.com>
2026-01-16 17:00:23 +0530

    testing v1

--- Backend/src/models/db.js
+++ Backend/src/models/db.js
@@ -16,6 +16,13 @@ const sequelize = new Sequelize(
         require: true,
         rejectUnauthorized: false
       }
+    },
+
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+      acquire: 30000
     }
   }
 );
